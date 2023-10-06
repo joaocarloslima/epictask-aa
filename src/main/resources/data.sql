dbbb7bf692847d702e9c7cfdad52d8f49730b894
Joao <joaocarloslima@me.com>
2023-10-06 11:55:40 -0300

    lista de tarefas

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO TASK (title, description, score) VALUES ('Prototipo', 'Criar o prototipo no figma', 50);
+INSERT INTO TASK (title, description, score) VALUES ('BD', 'Modelagem do bando de dados', 40);
+INSERT INTO TASK (title, description, score) VALUES ('Testes', 'Criar os testes do webapp', 60);
